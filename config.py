147bfcafcd5c0ddb0c8d7a663b38beb339380a3b
Varunkumaran522 <vk9623635@gmail.com>
2025-07-21 13:32:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://envs.sh/PXk.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6300548815').split()]
 
 PORT = os.environ.get("PORT", "8080")
